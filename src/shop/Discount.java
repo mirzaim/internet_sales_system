d38321497afe9d49acdf6685abbd4b7c7ebc3b55
Morteza <mortezamirzai120@gmail.com>
2019-04-03 11:39:11 +0430

    Add esential fields and methods to Discount class.

--- src/shop/Discount.java
+++ src/shop/Discount.java
@@ -1,4 +1,24 @@
 package shop;
 
 public class Discount {
+    private int id;
+    private int percent;
+    private Order goalOrder;
+
+    public Discount(int id, int percent) {
+        this.id = id;
+        this.percent = percent;
+    }
+
+    public void setGoalOrder(Order goalOrder) {
+        this.goalOrder = goalOrder;
+    }
+
+    public Order getGoalOrder() {
+        return goalOrder;
+    }
+
+    public int getPercent() {
+        return percent;
+    }
 }

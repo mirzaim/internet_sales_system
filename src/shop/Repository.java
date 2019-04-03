d3e87a37eaf3d5db30af11cf69d18c54d32ef39d
Morteza <mortezamirzai120@gmail.com>
2019-04-03 10:38:35 +0430

    Add esential fields and methods to Repository class.

--- src/shop/Repository.java
+++ src/shop/Repository.java
@@ -1,4 +1,34 @@
 package shop;
 
+import java.util.HashMap;
+
 public class Repository {
+    private int id;
+    private int capacity;
+    private HashMap<Good, Integer> goods;
+
+    public Repository(int id, int capacity) {
+        this.id = id;
+        this.capacity = capacity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public HashMap<Good, Integer> getGoods() {
+        return goods;
+    }
+
+    private int getFilledCapacity() {
+        int filledCapacity = 0;
+        for (HashMap.Entry<Good, Integer> entry : goods.entrySet()) {
+            filledCapacity += entry.getValue();
+        }
+        return filledCapacity;
+    }
 }
